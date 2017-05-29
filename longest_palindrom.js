17fcbcaaba92c21d5889236e8a76973a92223c5f
miketen <level1nightorc@yahoo.com>
2017-05-29 16:23:06 -0700

    30 minute session.

--- longest_palindrom.js
+++ longest_palindrom.js
@@ -1,4 +1,44 @@
-var longestPalindrom = function(str) {
-	// code here
+/**
+ * @param {string} s
+ * @return {string}
+ */
+ 
+ /*
 
-}
+Input: "babad"
+
+Output: "bab"
+
+Note: "aba" is also a valid answer.
+
+ */
+var longestPalindrome = function(s) {
+    var start = 0;
+    var end = s.length - 1;
+    
+    var string = "";
+    
+    
+    while (start < s.length && end > 0 && start < end) {
+        
+        var letterOne = s[start];
+        var letterTwo = s[end];
+        
+        
+        if (letterOne === letterTwo) {
+            string+= letterOne;
+        }
+        
+        start++;
+        end--;
+        
+        
+    }
+    
+    return string;
+    
+    
+   
+    
+    
+};
