19151524b73195fdacfe1c8f7b775f15381a44e4
miketen <level1nightorc@yahoo.com>
2017-06-05 21:16:09 -0700

    Medium problem, 15min session.

---
+++ searchmatrix.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[][]} matrix
+ * @param {number} target
+ * @return {boolean}
+ */
+var searchMatrix = function(matrix, target) {
+    
+    // binary search from top to botton?
+    
+    var mid = parseInt((matrix.length-1)/2);
+    var low = matrix[0][0];
+    var high = maxtrix[matrix.length-1][0];
+    
+    
+    // find the row where the number might exist
+    
+    // once row is found, then we search the row for the number
+    
+    
+};
